8689fa2583726a02dddc74863ddcef32fcc9411a
Eric Afes <eriicafes@gmail.com>
2022-07-02 09:52:42 +0100

    Add gin router

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello world")
+	router := gin.Default()
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"ping": "pong"})
+	})
+
+	router.Run()
 }
