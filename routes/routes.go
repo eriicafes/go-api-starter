b58dbeda3d5a55cf4ba78dd50c2851022d0f13db
Eric Afes <eriicafes@gmail.com>
2022-07-02 11:49:57 +0100

    Add routes abstraction package

---
+++ routes/routes.go
@@ -0,0 +1,31 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+type Controller interface {
+	Register(r *gin.RouterGroup)
+}
+
+type registry struct {
+	path        string
+	controller  Controller
+	middlewares []gin.HandlerFunc
+}
+
+func Register(path string, controller Controller, middlewares ...gin.HandlerFunc) registry {
+	return registry{
+		path:        path,
+		controller:  controller,
+		middlewares: middlewares,
+	}
+}
+
+func RegisterControllers(router *gin.Engine, controllers ...registry) {
+
+	for _, registry := range controllers {
+		registry.controller.Register(router.Group(
+			registry.path,
+			registry.middlewares...,
+		))
+	}
+}
