0c3f44f516b9361984e44090b59814606b6cc7c1
Eric Afes <eriicafes@gmail.com>
2022-07-05 02:05:38 +0100

    Add prototype database

---
+++ models/todo.go
@@ -0,0 +1,219 @@
+package models
+
+import (
+	"errors"
+	"time"
+
+	"github.com/eriicafes/filedb"
+)
+
+const TodoResource = "todos"
+
+type Todo struct {
+	ID          filedb.ID         `json:"id"`
+	Title       string            `json:"title"`
+	Description string            `json:"description"`
+	UserID      filedb.ForeignKey `json:"userId"`
+	CreatedAt   time.Time         `json:"createdAt"`
+	UpdatedAt   time.Time         `json:"updatedAt"`
+}
+
+type TodoQuery struct {
+	ID     int
+	Title  string
+	UserID int
+}
+
+type todoModel struct {
+	db *filedb.Database
+}
+
+func NewTodoModel(db *filedb.Database) *todoModel {
+	return &todoModel{db}
+}
+
+func matchTodoQuery(todo Todo, query *TodoQuery) bool {
+	if query == nil {
+		return true
+	}
+
+	matchID := todo.ID == filedb.ID(query.ID)
+	if query.ID == 0 {
+		matchID = true
+	}
+
+	matchTitle := todo.Title == query.Title
+	if query.Title == "" {
+		matchTitle = true
+	}
+
+	matchUserID := func() bool {
+		if todo.UserID != nil {
+			return *todo.UserID == filedb.ID(query.UserID)
+		}
+		return false
+	}()
+	if query.UserID == 0 {
+		matchUserID = true
+	}
+
+	return matchID && matchTitle && matchUserID
+}
+
+func (m *todoModel) get() []Todo {
+	var todos []Todo
+
+	m.db.Get(TodoResource, &todos)
+
+	return todos
+}
+
+func (m *todoModel) set(todos []Todo) {
+	var data []interface{}
+
+	for _, todo := range todos {
+		data = append(data, todo)
+	}
+
+	m.db.Set(TodoResource, data)
+}
+
+func (m *todoModel) FindOneTodo(query *TodoQuery) (*Todo, error) {
+	todos := m.get()
+
+	for _, todo := range todos {
+		match := matchTodoQuery(todo, query)
+
+		if match {
+			return &todo, nil
+		}
+	}
+
+	return nil, errors.New("todo not found")
+}
+
+func (m *todoModel) FindManyTodos(query *TodoQuery) []Todo {
+	todos := m.get()
+	var result []Todo
+
+	for _, todo := range todos {
+		match := matchTodoQuery(todo, query)
+
+		if match {
+			result = append(result, todo)
+		}
+	}
+
+	return result
+}
+
+func (m *todoModel) CreateTodo(todo Todo) *Todo {
+	todos := m.get()
+
+	// override fields
+	todo.ID = filedb.ID(len(todos) + 1)
+	todo.CreatedAt = time.Now()
+	todo.UpdatedAt = time.Now()
+
+	todos = append(todos, todo)
+
+	m.set(todos)
+
+	return &todo
+}
+
+func (m *todoModel) UpdateTodo(query *TodoQuery, updatedTodo Todo) (*Todo, error) {
+	todos := m.get()
+	var newTodos []Todo
+
+	var updated bool
+
+	for _, todo := range todos {
+		if updated {
+			newTodos = append(newTodos, todo)
+			continue
+		}
+
+		match := matchTodoQuery(todo, query)
+
+		if match {
+			updated = true
+
+			// override fields
+			updatedTodo.ID = todo.ID
+			updatedTodo.CreatedAt = todo.CreatedAt
+			updatedTodo.UpdatedAt = time.Now()
+			if updatedTodo.UserID == nil {
+				updatedTodo.UserID = todo.UserID
+			}
+
+			newTodos = append(newTodos, updatedTodo)
+			continue
+		}
+
+		newTodos = append(newTodos, todo)
+	}
+
+	if !updated {
+		return nil, errors.New("todo not found")
+	}
+
+	m.set(newTodos)
+	return &updatedTodo, nil
+}
+
+func (m *todoModel) RemoveOneTodo(query *TodoQuery) error {
+	todos := m.get()
+	var newTodos []Todo
+
+	var removed bool
+
+	for _, todo := range todos {
+		if removed {
+			newTodos = append(newTodos, todo)
+			continue
+		}
+
+		match := matchTodoQuery(todo, query)
+
+		if match {
+			removed = true
+			continue
+		}
+
+		newTodos = append(newTodos, todo)
+	}
+
+	if !removed {
+		return errors.New("todo not found")
+	}
+
+	m.set(newTodos)
+	return nil
+}
+
+func (m *todoModel) RemoveManyTodos(query *TodoQuery) (int, error) {
+	todos := m.get()
+	var newTodos []Todo
+
+	var count int
+
+	for _, todo := range todos {
+		match := matchTodoQuery(todo, query)
+
+		if match {
+			count++
+			continue
+		}
+
+		newTodos = append(newTodos, todo)
+	}
+
+	if count == 0 {
+		return count, errors.New("todos not found")
+	}
+
+	m.set(newTodos)
+
+	return count, nil
+}
---
+++ models/user.go
@@ -0,0 +1,204 @@
+package models
+
+import (
+	"errors"
+	"time"
+
+	"github.com/eriicafes/filedb"
+)
+
+const UserResource = "users"
+
+type User struct {
+	ID        filedb.ID `json:"id"`
+	Name      string    `json:"name"`
+	Age       int       `json:"age"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type UserQuery struct {
+	ID   int
+	Name string
+}
+
+type userModel struct {
+	db *filedb.Database
+}
+
+func NewUserModel(db *filedb.Database) *userModel {
+	return &userModel{db}
+}
+
+func matchUserQuery(user User, query *UserQuery) bool {
+	if query == nil {
+		return true
+	}
+
+	matchID := user.ID == filedb.ID(query.ID)
+	if query.ID == 0 {
+		matchID = true
+	}
+
+	matchName := user.Name == query.Name
+	if query.Name == "" {
+		matchName = true
+	}
+
+	return matchID && matchName
+}
+
+func (m *userModel) get() []User {
+	var users []User
+
+	m.db.Get(UserResource, &users)
+
+	return users
+}
+
+func (m *userModel) set(users []User) {
+	var data []interface{}
+
+	for _, user := range users {
+		data = append(data, user)
+	}
+
+	m.db.Set(UserResource, data)
+}
+
+func (m *userModel) FindOneUser(query *UserQuery) (*User, error) {
+	users := m.get()
+
+	for _, user := range users {
+		match := matchUserQuery(user, query)
+
+		if match {
+			return &user, nil
+		}
+	}
+
+	return nil, errors.New("user not found")
+}
+
+func (m *userModel) FindManyUsers(query *UserQuery) []User {
+	users := m.get()
+	var result []User
+
+	for _, user := range users {
+		match := matchUserQuery(user, query)
+
+		if match {
+			result = append(result, user)
+		}
+	}
+
+	return result
+}
+
+func (m *userModel) CreateUser(user User) *User {
+	users := m.get()
+
+	// override fields
+	user.ID = filedb.ID(len(users) + 1)
+	user.CreatedAt = time.Now()
+	user.UpdatedAt = time.Now()
+
+	users = append(users, user)
+
+	m.set(users)
+
+	return &user
+}
+
+func (m *userModel) UpdateUser(query *UserQuery, updatedUser User) (*User, error) {
+	users := m.get()
+	var newUsers []User
+
+	var updated bool
+
+	for _, user := range users {
+		if updated {
+			newUsers = append(newUsers, user)
+			continue
+		}
+
+		match := matchUserQuery(user, query)
+
+		if match {
+			updated = true
+
+			// override fields
+			updatedUser.ID = user.ID
+			updatedUser.CreatedAt = user.CreatedAt
+			updatedUser.UpdatedAt = time.Now()
+
+			newUsers = append(newUsers, updatedUser)
+			continue
+		}
+
+		newUsers = append(newUsers, user)
+	}
+
+	if !updated {
+		return nil, errors.New("user not found")
+	}
+
+	m.set(newUsers)
+	return &updatedUser, nil
+}
+
+func (m *userModel) RemoveOneUser(query *UserQuery) error {
+	users := m.get()
+	var newUsers []User
+
+	var removed bool
+
+	for _, user := range users {
+		if removed {
+			newUsers = append(newUsers, user)
+			continue
+		}
+
+		match := matchUserQuery(user, query)
+
+		if match {
+			removed = true
+			continue
+		}
+
+		newUsers = append(newUsers, user)
+	}
+
+	if !removed {
+		return errors.New("user not found")
+	}
+
+	m.set(newUsers)
+	return nil
+}
+
+func (m *userModel) RemoveManyUsers(query *UserQuery) (int, error) {
+	users := m.get()
+	var newUsers []User
+
+	var count int
+
+	for _, user := range users {
+		match := matchUserQuery(user, query)
+
+		if match {
+			count++
+			continue
+		}
+
+		newUsers = append(newUsers, user)
+	}
+
+	if count == 0 {
+		return count, errors.New("users not found")
+	}
+
+	m.set(newUsers)
+
+	return count, nil
+}
